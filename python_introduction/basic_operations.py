01a0b5b1e80b963e11358a5f4cf597a9d6ff1596
Felix Kuffour <Jayycrypt@gmail.com>
2025-07-13 23:02:28 +0000

    python basics

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+x= number1 + number2
+y= number1 - number2
+z = number1 * number2
+
+print("Addition of 10 and 5 is", x)
+print("Subtraction of 10 and 5 is", y)
+print("Multiplication of 10 and 5 is", z)
