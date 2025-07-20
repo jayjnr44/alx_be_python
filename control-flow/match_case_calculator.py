a33b10c4140444c55d30706d2697b8082c010a56
Felix Kuffour <Jayycrypt@gmail.com>
2025-07-20 21:56:02 +0000

    control flows in python

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":  
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: Division by zero is not allowed."
+    case _:
+        result = "Error: Invalid operation."
+print("The result is: ", result)
