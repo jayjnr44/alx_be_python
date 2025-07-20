863e2bae340cbfc017469c1d7b89fbb6bd871ac3
Felix Kuffour <Jayycrypt@gmail.com>
2025-07-20 20:58:43 +0000

    deleted

--- MatDT.py
+++
@@ -1,9 +0,0 @@
-value = input("Enter a value (number or string): ")
-
-match value:
-    case int():
-        print("you entered an integer:", value)
-    case str():
-        print("You entered a string:", value)
-    case _:
-        print("You entered an invalid data type")
