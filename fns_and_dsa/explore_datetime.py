538bba720325d1ce9f8bd33d9e0589924295c63b
Felix Kuffour <Jayycrypt@gmail.com>
2025-07-23 10:59:53 +0000

    fucntions and dsa in python

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,8 +12,8 @@ display_current_datetime()
 No_of_days = int(input("Enter number of days to add to the current date: "))
 
 def calculate_future_date():
-    future_date = datetime.datetime.today() + timedelta(days=No_of_days)
-    formatted_future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    future_date = datetime.date.today() + timedelta(days=No_of_days)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
     print(f"Future date: {formatted_future_date}")
 
 calculate_future_date()
