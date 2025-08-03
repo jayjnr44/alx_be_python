c385d8980bfe205b85e7cc204080485fe9211668
Felix Kuffour <Jayycrypt@gmail.com>
2025-08-03 23:40:16 +0000

    programming paradigms in python

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,7 +20,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-5, 3), -8)
         self.assertEqual(self.calc.subtract(2.5, 1.0), 1.5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
         self.assertEqual(self.calc.multiply(0, 100), 0)
