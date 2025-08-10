27a7cbdb70829a2b69178a588085523d8601d957
Felix Kuffour <Jayycrypt@gmail.com>
2025-08-10 12:11:32 +0000

    oop methods in python

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -11,7 +11,7 @@ class Rectangle(Shape):
         self.width = width
 
     def area(self):
-        return self.width * self.height
+        return self.length * self.width
 
 class Circle(Shape):
     def __init__(self, radius):
